72c7d432f653200feac71008e950f97f6cbea90b
Ruzgfpegk <Ruzgfpegk@zero-sum.fr>
2023-12-03 02:42:43 +0100

    Jour 1.

---
+++ 1-1.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+)
+
+// https://adventofcode.com/2023/day/1
+// Sans les substitutions (partie 1) : 57346
+
+func main() {
+	file, err := os.Open("1_calibration.txt")
+	//file, err := os.Open("1_example1.txt") // 142, sans substitutions
+	if err != nil {
+		fmt.Println("Erreur lors de l'ouverture du fichier: ", err)
+		return
+	}
+	defer file.Close()
+
+	reFinalMatch := regexp.MustCompile(`(\d).*?(\d)?[^0-9]*$`)
+
+	scanner := bufio.NewScanner(file)
+
+	var total = 0
+	var lineNum = 0
+
+	for scanner.Scan() {
+		lineNum++
+		line := scanner.Text()
+
+		matches := reFinalMatch.FindStringSubmatch(line)
+
+		if matches != nil {
+			if matches[2] == "" {
+				matches[2] = matches[1]
+			}
+
+			lineFinal, _ := strconv.Atoi(matches[1] + matches[2])
+			total += lineFinal
+			//fmt.Println(lineNum, " - ", total, " - ", lineFinal)
+		} else {
+			fmt.Println("Erreur ligne ", lineNum)
+			return
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier: ", err)
+		return
+	}
+
+	fmt.Println("Total: ", total)
+}
---
+++ 1-2.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+// https://adventofcode.com/2023/day/1
+// Avec les substitutions (partie 2) : 57345
+
+func main() {
+	file, err := os.Open("1_calibration.txt")
+	//file, err := os.Open("1_example2.txt") // 281, avec substitutions
+	if err != nil {
+		fmt.Println("Erreur lors de l'ouverture du fichier: ", err)
+		return
+	}
+	defer file.Close()
+
+	// Faits de telle sorte que le "remplacement" d'un mot n'en coupe par un autre
+	numsLong := [9]string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	numsShort := [9]string{"1e", "2o", "3e", "4", "5e", "6", "7n", "8t", "9e"}
+
+	reFinalMatch := regexp.MustCompile(`(\d).*?(\d)?[^0-9]*$`)
+
+	scanner := bufio.NewScanner(file)
+
+	var total = 0
+	var lineNum = 0
+
+	for scanner.Scan() {
+		lineNum++
+		line := scanner.Text()
+
+		// Remplacement de texte
+		for i := 0; i < len(line); i++ { // Itération dans la ligne
+			//subLine = line[i:] // On commence la recherche à la colonne actuelle
+			for j := 0; j < 9; j++ { // Itération dans les nombres
+				if strings.Index(line, numsLong[j]) == i {
+					line = strings.ReplaceAll(line, numsLong[j], numsShort[j]) // Injection du nombre court
+				}
+			}
+		}
+
+		matches := reFinalMatch.FindStringSubmatch(line)
+
+		if matches != nil {
+			if matches[2] == "" {
+				matches[2] = matches[1]
+			}
+
+			lineFinal, _ := strconv.Atoi(matches[1] + matches[2])
+			total += lineFinal
+			//fmt.Println(lineNum, " - ", total, " - ", lineFinal)
+		} else {
+			fmt.Println("Erreur ligne ", lineNum)
+			return
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Erreur lors de la lecture du fichier: ", err)
+		return
+	}
+
+	fmt.Println("Total: ", total)
+}
